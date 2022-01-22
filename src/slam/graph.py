a438cc46faeb4463e241867c479d661151cd6411
yoraish <yorai.shaoul@gmail.com>
2022-01-22 10:25:49 +0200

    feat(slam): wip.

--- src/slam/graph.py
+++ src/slam/graph.py
@@ -4,9 +4,9 @@ class Edge():
     def __init__(self, i, j, xj, yj, tj, cost) -> None:
         self.i = i
         self.j = j
-        self.dx = xj
-        self.dy = yj
-        self.dt = tj
+        self.x = xj
+        self.y = yj
+        self.t = tj
         self.cost = cost
 
 class Graph():
@@ -50,18 +50,16 @@ class Graph():
 
         
 
-    def add_edge(self, i, dx, dy, dt):
+    def add_edge(self, i, j, dx, dy, dt):
 
         """Add an edge to the pose graph.
 
         Args:
-            xi_ix (int): the index of the previous node. Edge is from x_i to x_j, all quantities in the reference frame of x_i.
+            xi_ix (int): the index of the previous node. Edge is from x_i to x_j, all quantities in the reference frame of x_i. If i == j then this is an assignment, and the x,y,t quantities are in the map frame.
             dx (float): x axis displacement between x_i.x to this new observation.
             dy (float): y axis displacement between x_i.y to this new obserevation.
             dt (float): angle radians change between x_i.t to this new observation. In range [-pi, +pi].
         """
-        # The index of the new node.
-        j = len(self.A[0])
         # Decide what cost to use.
         if i == j:
             cost = self.ASSIGNMENT_COST
@@ -72,7 +70,7 @@ class Graph():
 
         # Record edge.
         self.edges.append(Edge(i, j, dx, dy, dt, cost))
-
+        return 
         if self.A is None and i == 0:
             print("FIRST EDGE INITIALIZATION")
             self.A = np.array([[1]]) * self.ASSIGNMENT_COST
@@ -108,27 +106,72 @@ class Graph():
                     # Expand the xy vector.
                     # self.C = np.hstack((self.C, np.zeros((self.C.shape[0], 1))))
                     # self.C = np.vstack((self.C, np.zeros((1, self.C.shape[1]))))
-                    # self.d = np.vstack((self.A, np.array([tj]) * cost))
+                    # self.d = np.vstack((self.d, np.array([tj]) * cost))
         '''
 
     def optimize(self):
         # Build the angles matrix and vector.
         # Initialize.
+        if self.edges[0].i != self.edges[0].j:
+            raise RuntimeError("First edge should be an assignment edge, not a cross-node edge.")
+        
         self.A = np.array([[1]]) * self.edges[0].cost
-        self.b = np.array([[self.edges[0].tj]]) * self.edges[0].cost
+        self.b = np.array([[self.edges[0].t]]) * self.edges[0].cost
 
         for j in range(1, len(self.edges)):
-            edge = self.edges[j] # From i to j. xyt are for j and in map frame.
+            edge = self.edges[j] # From i to j. xyt are for j and in i frame. If i==j then xyt are in the map frame.
+
             # Expand the angle association matrix with zeros.
             self.A = np.hstack((self.A, np.zeros((self.A.shape[0], 1))))
             self.A = np.vstack((self.A, np.zeros((1, self.A.shape[1]))))
 
             # Expand the angle vector.
-            self.b = np.vstack((self.b, np.array([edge.tj]) * edge.cost))
+            self.b = np.vstack((self.b, np.array([edge.t]) * edge.cost))
 
-            # Expand the angle matrix.
+            # Update the angle matrix.
             self.A[-1, edge.i] = -1.0 * edge.cost
             self.A[-1, edge.j] = 1.0  * edge.cost
+        
+        # Optimize for angles.
+        t_hat = np.linalg.pinv(self.A).dot(self.b)
+
+        print(t_hat)
+
+        # Build the xy matrix and vector.
+        self.C = np.array([[1, 0],
+                            [0, 1]]) * self.edges[0].cost
+        self.d = np.array([[self.edges[0].x], [self.edges[0].y]]) * self.edges[0].cost
+
+        for j in range(1, len(self.edges)):
+            edge = self.edges[j]
+
+            # Expand the xy matrix with two columns and two rows.
+            self.C = np.hstack((self.C, np.zeros((self.C.shape[0], 2))))
+            self.C = np.vstack((self.C, np.zeros((2, self.C.shape[1]))))
+
+            # Expand the observation xy vector.
+            self.d = np.vstack((self.d, np.array([[edge.x], [edge.y]]) * edge.cost))
+
+            # Update the xy matrix. 
+            # If this is an assignment edge, mark ones and zeroes appropriately.
+            if edge.i == edge.j:
+                self.C[-2, 2*edge.j]     = 1 * edge.cost # The x value.
+                self.C[-1, 2*edge.j + 1] = 1 * edge.cost # The y value.
+            
+            else:
+                # If i is not in the graph yet, add two more columns since we have 2 more variables (x_i,y_i, x_j, y_j) as opposed to only x_j, y_j.
+                if edge.i >= len(self.C):
+                    self.C = np.hstack((self.C, np.zeros((self.C.shape[0], 2))))
+
+                    
+                # The equation for x_j.
+                self.C[-2, 2*edge.i]     = -np.cos(t_hat[edge.i]) * edge.cost # The x value of i. TODO(yoraish): add the other terms from  j to xi yi.
+                self.C[-2, 2*edge.i + 1] = -np.sin(t_hat[edge.i]) * edge.cost # The y value of i.
+                xj
+                yj
+
+                # The equation for y_j
+                self.C[-1, 2*edge.j + 1] = 1 * edge.cost # The y value.
 
 
 
--- src/slam/world.py
+++ src/slam/world.py
@@ -2,5 +2,8 @@ import numpy as np
 from graph import Graph
 
 g = Graph()
-g.initialize(0,0,0)
-g.add_edge(1, 1, 10, 10, 1)
+g.add_edge(0, 0, 10, 10, 0)
+g.add_edge(1, 1, 10, 10, 0)
+g.add_edge(2, 2, 10, 10, np.pi)
+g.add_edge(3, 3, 10, 10, 0)
+g.optimize()
