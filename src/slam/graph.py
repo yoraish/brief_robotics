29e5bbe1502e9db595e0c0ffa3260d67138db76a
yoraish <yorai.shaoul@gmail.com>
2022-01-31 12:07:06 -0500

    feat(slam): poc.

--- src/slam/graph.py
+++ src/slam/graph.py
@@ -1,17 +1,21 @@
 import numpy as np
 
 class Edge():
-    def __init__(self, i, j, xj, yj, tj, cost) -> None:
+    def __init__(self, i, j, xj, yj, tj, cost):
         self.i = i
         self.j = j
-        self.x = xj
+        # The following are in i frame.
+        self.x = xj 
         self.y = yj
         self.t = tj
         self.cost = cost
+    
+    def __str__(self):
+        return "i: %d, j: %d x: %d y: %d t: %d cost: %d" %(self.i, self.j, self.x, self.y, self.t, self.cost)
 
 class Graph():
 
-    def __init__(self) -> None:
+    def __init__(self):
         # Let there be n observations.
         # For optimizing angles. Find hat_t to min ||A hat_t - b||
         self.A = None # nxn
@@ -23,17 +27,25 @@ class Graph():
 
         # Costs.
         self.ODOM_COST = 1
-        self.LANDMARK_COST = 100
-        self.ASSIGNMENT_COST = 10000
+        self.LANDMARK_COST = 300
+        self.ASSIGNMENT_COST = 200
 
         # Landmarks.
-        self.assigned = set()
+        self.landmarks = set()
         
         # Keep track of xy edges and build matrix C only after optimizing for angles.
         self.edges = []
 
         # Should we optimize after every edge addition?
         self.online = False
+
+        # Optimized poses.
+        self.x_hat, self.t_hat = None, None
+
+        # Visualization.
+        self.arrow_length = 0.05
+        self.arrow_width = 0.001
+        self.prior_xyt = {}
 
     def initialize(self, x, y, t):
         """Adds the first node to the graph whith set value.
@@ -48,9 +60,8 @@ class Graph():
         
 
 
-        
 
-    def add_edge(self, i, j, dx, dy, dt):
+    def add_edge(self, i, j, dx, dy, dt, prior_xyt = None):
 
         """Add an edge to the pose graph.
 
@@ -63,13 +74,20 @@ class Graph():
         # Decide what cost to use.
         if i == j:
             cost = self.ASSIGNMENT_COST
-        elif i in self.assigned:
+            self.landmarks.add(i)
+        elif i in self.landmarks:
             cost = self.LANDMARK_COST
         else:
             cost = self.ODOM_COST
 
+
         # Record edge.
         self.edges.append(Edge(i, j, dx, dy, dt, cost))
+
+        # Optionally add a visualization prior.
+        if prior_xyt is not None:
+            self.prior_xyt[j] = prior_xyt
+
         return 
         if self.A is None and i == 0:
             print("FIRST EDGE INITIALIZATION")
@@ -165,22 +183,104 @@ class Graph():
 
                     
                 # The equation for x_j.
-                self.C[-2, 2*edge.i]     = -np.cos(t_hat[edge.i]) * edge.cost # The x value of i. TODO(yoraish): add the other terms from  j to xi yi.
-                self.C[-2, 2*edge.i + 1] = -np.sin(t_hat[edge.i]) * edge.cost # The y value of i.
-                xj
-                yj
+                self.C[-2, 2*edge.i]     = -np.cos(t_hat[edge.i]) * edge.cost 
+                self.C[-2, 2*edge.i + 1] = -np.sin(t_hat[edge.i]) * edge.cost 
+                self.C[-2, 2*edge.j]     =  np.cos(t_hat[edge.i]) * edge.cost 
+                self.C[-2, 2*edge.j + 1] =  np.sin(t_hat[edge.i]) * edge.cost 
 
-                # The equation for y_j
-                self.C[-1, 2*edge.j + 1] = 1 * edge.cost # The y value.
-
-
-
-
- 
-
-        # Expand the 
+                # The equation for y_j.
+                self.C[-1, 2*edge.i]     =  np.sin(t_hat[edge.i]) * edge.cost  
+                self.C[-1, 2*edge.i + 1] = -np.cos(t_hat[edge.i]) * edge.cost 
+                self.C[-1, 2*edge.j]     = -np.sin(t_hat[edge.i]) * edge.cost
+                self.C[-1, 2*edge.j + 1] =  np.cos(t_hat[edge.i]) * edge.cost 
 
 
+        print(self.C)
+        # Solve for the positions.
+        x_hat = np.linalg.pinv(self.C).dot(self.d)
+        self.x_hat, self.t_hat = x_hat, t_hat
+        return x_hat, t_hat
 
 
+    def visualize(self):
+        import matplotlib.pyplot as plt
+        fig = plt.figure(0)
+        ax = fig.add_subplot(111)
 
+        # Show the unoptimized poses.
+        poses = {}
+        for edge in self.edges:
+            i, j, dx, dy, dt = edge.i, edge.j, edge.x, edge.y, edge.t
+
+            # If this is an assignment edge (self-edge), just draw it.
+            if i == j:
+                ax.arrow(dx, dy, self.arrow_length * np.cos(dt), self.arrow_length * np.sin(dt), width=self.arrow_width, color = "b", alpha = 0.2)
+                poses[j] = (dx, dy, dt)
+
+            else:
+                # Take the pose of i and transform it to j. If i does not exist, disregard it. This means that the plot will only start after seeing at least one landmark. At indoor this is okay since the tile is a landmark.
+                if i not in poses.keys():
+                    print("[VIS] Skipping i", i, "j", j, "since there is still no i.")
+                    continue
+                if i in self.landmarks:
+                    c = 'b'
+                else:
+                    c = 'r'
+
+                # If already have an estimate for j, draw that one.
+                if j in poses.keys():
+                    ax.plot([poses[i][0] ,poses[j][0]], [poses[i][1], poses[j][1]], color = c, alpha = 0.2)
+                    continue
+                
+                # If no estimate for j, compute from the relative transform form i.
+                else:
+                    R = np.array([[np.cos(poses[i][2]), -np.sin(poses[i][2])], 
+                                [np.sin(poses[i][2]), np.cos(poses[i][2])]])
+
+                    trans_j = R.dot(np.array([[dx], [dy]])) + np.array([[poses[i][0]], [poses[i][1]]])
+                    rot_j = poses[i][2] + dt
+                    poses[j] = (trans_j[0][0], trans_j[1][0], rot_j)
+
+                    # Draw in red and if i is a landmark then draw blue line the landmark.
+                    ax.arrow(trans_j[0][0], trans_j[1][0], self.arrow_length * np.cos(rot_j), self.arrow_length * np.sin(rot_j), width=self.arrow_width, color = "r", alpha = 0.2)
+
+                    ax.plot([poses[i][0] ,trans_j[0][0]], [poses[i][1], trans_j[1][0]], color = c, alpha = 0.2)
+
+                    # Draw prior if it exists.
+                    if j in self.prior_xyt:
+                        x,y,t = self.prior_xyt[j]
+                        ax.arrow(x, y, self.arrow_length * np.cos(t), self.arrow_length * np.sin(t), width=self.arrow_width, color = "g", alpha = 0.2)
+                        ax.scatter([x],[y])
+        ax.set_aspect("equal")
+    
+
+
+        # Show the optimized points.
+        # Check if optimization has already happened.
+        if self.x_hat is not None:
+            for i in range(len(self.t_hat)):
+                if i in self.landmarks:
+                    c = "b"
+                else:
+                    c = "r"
+                ax.arrow(self.x_hat[2*i][0], self.x_hat[2*i + 1][0], self.arrow_length * np.cos(self.t_hat[i][0]), self.arrow_length * np.sin(self.t_hat[i][0]), width=self.arrow_width, color = c)
+                
+            # Go through all the edges and draw some lines (red between poses and blue for poses-to-landmarks.)
+            for edge in self.edges:
+                i, j = edge.i, edge.j
+                if i in self.landmarks:
+                    c = 'b'
+                else:
+                    c = 'r'
+                    
+                ax.plot([self.x_hat[2*i][0] ,self.x_hat[2*j][0]], [self.x_hat[2*i + 1][0], self.x_hat[2*j + 1][0]], color = c)
+                ax.plot([self.x_hat[2*i][0] ,self.x_hat[2*j][0]], [self.x_hat[2*i + 1][0], self.x_hat[2*j + 1][0]], color = c)
+
+                # Draw a line between the original and the optimized poses.
+                ax.plot( [self.x_hat[2*j][0] ,poses[j][0]], [self.x_hat[2*j + 1][0], poses[j][1]], color = 'k', linestyle = "dotted", alpha = 0.2)
+
+
+        ax.set_title("""self.ODOM_COST = %d
+        self.LANDMARK_COST = %d
+        self.ASSIGNMENT_COST = %d""" % (self.ODOM_COST, self.LANDMARK_COST, self.ASSIGNMENT_COST))
+        plt.show()
--- src/slam/world.py
+++ src/slam/world.py
@@ -1,9 +1,168 @@
+from cv2 import sort
 import numpy as np
 from graph import Graph
+import yaml
+import rosbag
 
+
+def relative_transform(xi, yi, ti, xj, yj, tj):
+    # Returns the pose of j in the frame of i.
+    R = np.array([[np.cos(ti), -np.sin(ti)], 
+                  [np.sin(ti), np.cos(ti)]])
+    shifted_j = np.array([[xj], [yj]]) - np.array([[xi], [yi]])
+    j_in_i = R.T.dot(shifted_j)
+    # print(xi, yi, ti, xj, yj, tj)
+    # print("-->", j_in_i[0][0], j_in_i[1][0], tj - ti)
+    # import matplotlib.pyplot as plt
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    # ax.arrow(xi, yi, 0.1 * np.cos(ti), 0.1*np.sin(ti), color = 'b')
+    # ax.arrow(xj, yj, 0.1 * np.cos(tj), 0.1*np.sin(tj), color = 'r')
+    # ax.scatter([xi,xj], [yi, yj])
+    # ax.set_aspect("equal")
+    # plt.show()
+    return j_in_i[0][0], j_in_i[1][0], tj - ti
+
+def deg_to_rad(deg):
+    rad = deg/360.0*2*np.pi
+    rad = ((rad + np.pi) % (2 * np.pi) ) - np.pi
+    return rad
+
+def quat_to_rad(x,y,z,w):
+    import math
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw = math.atan2(t3, t4)
+    return yaw
+
+'''
 g = Graph()
-g.add_edge(0, 0, 10, 10, 0)
-g.add_edge(1, 1, 10, 10, 0)
-g.add_edge(2, 2, 10, 10, np.pi)
-g.add_edge(3, 3, 10, 10, 0)
+# Add landmarks.
+g.add_edge(0, 0, 15, 15, np.pi/2.)
+g.add_edge(1, 1, 11, 11, np.pi/2.)
+g.add_edge(2, 2, 12, 12, np.pi/2.)
+g.add_edge(3, 3, 1, 1, 0)
+
+# Add odometry measurements and landmark observations.
+g.add_edge(3, 4, 3, 0, 0.2)
+g.add_edge(4, 5, 3, 0, 0.2)
+g.add_edge(5, 6, 3, 0, 0.2)
+g.add_edge(6, 7, 3, 0, 0.2)
+g.add_edge(0, 7, -1, 0, 0)
+
+
+# # If this is not an assignment edge, then we should compute the j pose in the i frame.
+# if i != j:
+#     dx, dy, dt = self.get_relative_transform()
+
 g.optimize()
+g.visualize()
+'''
+
+# ==========================
+# RUNNING THE WORLD (girls).
+# ==========================
+if __name__ == "__main__":
+    bagfile_path = "/home/yoraish/Desktop/yorai/data/bagfiles/28-135-production_2022-01-28-09-23-57.bag"
+    beacons_yaml_path = '/indoorRobotics/indoor_maps/production/28/beacons.yaml'
+
+    g = Graph()
+    # Start with getting the beacons of the zone.
+    with open (beacons_yaml_path , "r") as stream:
+        try:
+            beacons_data = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    # Sort the beacons by name.
+    sorted_beacons = [beacon_key for beacon_key in beacons_data.keys()]
+    sorted_beacons.sort( key = lambda x: int(x.split("_")[-1]))
+    beacon_num = -1
+    for beacon_key in sorted_beacons:
+        new_beacon_num = int(beacon_key.split("_")[-1])
+
+        if new_beacon_num > beacon_num + 1:
+            while new_beacon_num > beacon_num + 1:
+                beacon_num += 1
+                g.add_edge(beacon_num, beacon_num, 0, 0, 0)
+                print("Adding empty space for skipped beacon at %d." % beacon_num)
+            beacon_num += 1
+            print("Done adding empty. beacon_num at %d" % beacon_num)
+        else:
+            beacon_num = new_beacon_num
+
+        x, y, _, t, _ = beacons_data[beacon_key]
+        t = deg_to_rad(t)
+        print(beacon_num, x,y,t)
+
+        g.add_edge(beacon_num, beacon_num, x, y, t)
+
+    
+
+
+    # Get poses. 
+    # Whenever we get a beacon sighting, if the most recent location pose was received less than 100ms ago, we connect an edge from the landmark to the pose.
+    recent_location_xyt = None
+    recent_location_time = None
+    recent_pose_ix = g.edges[-1].j
+    odom_is_rooted = False # We require one landmark observation to be added before adding odom.
+
+    # Landmark recently around.
+    ldx = None
+    ldy = None
+    ldt = None
+    recent_landmark_time = 0
+    lix = None
+
+    bag = rosbag.Bag(bagfile_path)
+    for topic, msg, t in bag.read_messages(topics=['/indoor/monitor/location', '/indoor/beacon/pose', '/indoor/mission/status']):
+
+        if topic == "/indoor/monitor/location":
+
+            # If no landmark seen yet, do not add an odom pose.
+            if odom_is_rooted == False:
+                recent_location_xyt = [msg.location.x, msg.location.y, deg_to_rad(msg.heading)]
+                recent_location_time = t.to_sec()
+                # Only root the odometry measurements when a link to a landmark is formed. This happens when a landmark measurement is recorded less than 100ms before an odom measurement.
+                if t.to_sec() - recent_landmark_time < 0.1:
+                    # Add a new pose with an edge to this landmark. The new pose gets the ix recent_pose_ix + 1
+                    j = recent_pose_ix + 1
+                    recent_pose_ix = j
+                    recent_location_time = t.to_sec()
+                    odom_is_rooted = True
+                    g.add_edge(lix, j, ldx, ldy, ldt, [msg.location.x, msg.location.y, deg_to_rad(msg.heading)])
+
+            # If we have previous landmark positions and previous poses, then add an edge between the previous location and this new one.
+            else:
+                i = recent_pose_ix
+                j = recent_pose_ix + 1
+                recent_pose_ix = j
+                recent_location_time = t.to_sec()
+                dx, dy, dt = relative_transform(*(recent_location_xyt + [msg.location.x, msg.location.y, deg_to_rad(msg.heading)]))
+                g.add_edge(i, j, dx, dy, dt, [msg.location.x, msg.location.y, deg_to_rad(msg.heading)])
+                recent_location_xyt = [msg.location.x, msg.location.y, deg_to_rad(msg.heading)]
+
+                if t.to_sec() - recent_landmark_time < 0.01:
+                    # Add a new pose with an edge to this landmark. The new pose gets the ix recent_pose_ix + 1
+                    g.add_edge(lix, j, ldx, ldy, ldt, [msg.location.x, msg.location.y, deg_to_rad(msg.heading)])
+
+            
+        if topic == "/indoor/beacon/pose" and odom_is_rooted == False:
+            # Transform from beacon to robot.
+            ldx = msg.pose.pose.position.x
+            ldy = msg.pose.pose.position.y
+            ldt = quat_to_rad(msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w)
+            recent_landmark_time = t.to_sec()
+            lix = int(msg.header.frame_id[len("beacon"):])
+
+            # # If the most recent location was seen less than 100ms ago, then create an edge between this beacon and that most recent location.
+            # if recent_location_xyt is not None:
+            #     if t.to_sec() - recent_location_time < 0.1:
+            #         odom_is_rooted = True
+
+        if len(g.edges) > 26:
+            break
+    bag.close()
+
+    g.optimize()
+    g.visualize()
