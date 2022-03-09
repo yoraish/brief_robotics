dc16aa543468c94b8611d223ab700e73226658bd
yoraish <yorai.shaoul@gmail.com>
2022-03-09 10:44:33 +0200

    feat(slam): gtsam on bagfile.

--- src/slam/gtgraph.py
+++ src/slam/gtgraph.py
@@ -1,6 +1,7 @@
 import gtsam
 import numpy as np
 from gtsam.symbol_shorthand import L, X
+from trimesh import transform_points
 
 
 class GTEdge():
@@ -16,6 +17,14 @@ class GTEdge():
     def __str__(self):
         return "i: %d, j: %d x: %d y: %d t: %d cost: %d" %(self.i, self.j, self.x, self.y, self.t, self.noise)
 
+class RangeMeasurement():
+    def __init__(self, r, l):
+        self.right = r
+        self.left = l
+    
+    def __str__(self):
+        return "Measurement Left: %d, Right: %d" %(self.left, self.right)
+
 class Graph():
 
     def __init__(self):
@@ -27,6 +36,9 @@ class Graph():
 
         # Landmarks.
         self.landmarks = set()
+
+        # Measurements. Mapping nodes to RangeMeasurement's/
+        self.measurements = {}
         
         # Keep track of xy edges and build matrix C only after optimizing for angles.
         self.edges = []
@@ -38,7 +50,7 @@ class Graph():
         self.graph = gtsam.NonlinearFactorGraph()
         self.graph_keys = {} # Maps node id to gtsam symbol.
         self.initial_estimate = gtsam.Values()
-
+        self.prior_xyt = {} # Node id : (x,y,t)
 
         # Optimized poses.
         self.x_hat, self.t_hat = None, None
@@ -46,8 +58,10 @@ class Graph():
         # Visualization.
         self.arrow_length = 0.05
         self.arrow_width = 0.001
-        self.prior_xyt = {} # Node id : (x,y,t)
-
+        self.map_path =  "/indoorRobotics/indoor_maps/production/28/rviz_map.pgm"
+        self.map_resolution = 0.05 # Meters per pixel.
+        self.map_width_pixels  = 0
+        self.map_height_pixels = 0
 
     def normalize_yaw(self, yaw):
         return ( yaw + np.pi ) % (2 * np.pi) - np.pi
@@ -60,6 +74,9 @@ class Graph():
         j_in_i = R.T.dot(shifted_j)
         return j_in_i[0][0], j_in_i[1][0], tj - ti
 
+    def add_measurement(self, j, m):
+        # Adds a measurement (RangeMeasurement object) that was observed at node j.
+        self.measurements[j] = m
 
     def add_edge(self, i, j, dx, dy, dt, prior_xyt):
 
@@ -137,58 +154,118 @@ class Graph():
         self.t_hat = [ [result.atPose2(self.graph_keys[j]).theta() ] for j in range(len(self.prior_xyt))]
         return self.x_hat, self.t_hat
 
+    def transform_measurements(self, poses):
+        # With poses being a list of (x,y,t) and observations being a map between index j (in poses): RangeMeasurement object, return the map coordinates of the measurements.
+        transformed_measurements = []
+        for j in poses:
+            # Add measurement to maps figure.
+            if j in self.measurements:
+                if self.measurements[j].right != 0:
+                    # Transformation for j in map frame.
+                    j_in_map = np.array([[np.cos(poses[j][2]), -np.sin(poses[j][2]), poses[j][0]], 
+                                         [np.sin(poses[j][2]),  np.cos(poses[j][2]), poses[j][1]],
+                                         [0                  , 0                   , 1]])
 
-    def visualize(self):
+
+                    right_in_j_homo = np.array([[0, self.measurements[j].right, 1]]).T
+                    left_in_j_homo  = np.array([[0, -self.measurements[j].left, 1]]).T
+                    right_in_map = j_in_map.dot(right_in_j_homo)
+                    left_in_map = j_in_map.dot(left_in_j_homo)
+                    # Append (x,y) tuples to rights/lefts depending on the origin of the point.
+                    transformed_measurements.append([right_in_map[0][0], right_in_map[1][0]])
+                    transformed_measurements.append([left_in_map[0][0], left_in_map[1][0]])
+                    # ax_maps.scatter([right_in_map[0][0]], [right_in_map[1][0]], color = c)
+                    # ax_maps.scatter([left_in_map[0][0]], [left_in_map[1][0]], color = c)
+        return transformed_measurements
+
+    def visualize(self, additional_points = []): # Additional points is a list of tuples (x,y) or (x,y,t).
         import matplotlib.pyplot as plt
         fig = plt.figure(0)
         ax = fig.add_subplot(111)
 
-        # Show the unoptimized poses.
-        poses = {}
+        # Also create figures for the generated maps.
+        fig_maps = plt.figure(1)
+        ax_maps = fig_maps.add_subplot(111)
+
+        # Store tof points from right/left for map visualization.
+        rights, lefts = [], []
+
+        # Containers for the odom/optimized points in another form. Mostly for visualizing maps. This includes landmarks.
+        odom_poses = {} # j:(x,y,t)
+        gt_poses = {} # j:(x,y,t)
+
         for edge in self.edges:
             i, j, dx, dy, dt = edge.i, edge.j, edge.x, edge.y, edge.t
 
             # If this is an assignment edge (self-edge), just draw it.
             if i == j:
                 ax.arrow(dx, dy, self.arrow_length * np.cos(dt), self.arrow_length * np.sin(dt), width=self.arrow_width, color = "b", alpha = 0.2)
-                poses[j] = (dx, dy, dt)
+                odom_poses[j] = (dx, dy, dt)
                 ax.scatter(dx, dy)
+                ax.text(dx, dy, "beacon"+str(i))
+                
 
             else:
                 # Take the pose of i and transform it to j. If i does not exist, disregard it. This means that the plot will only start after seeing at least one landmark. At indoor this is okay since the tile is a landmark.
-                if i not in poses.keys():
+                if i not in odom_poses.keys():
                     print("[VIS] Skipping i", i, "j", j, "since there is still no i.")
                     continue
                 if i in self.landmarks:
                     c = 'b'
                 else:
-                    c = 'r'
+                    c = 'y'
 
                 # If already have an estimate for j, draw that one.
-                if j in poses.keys():
-                    ax.plot([poses[i][0] ,poses[j][0]], [poses[i][1], poses[j][1]], color = c, alpha = 0.2)
+                if j in odom_poses.keys():
+                    ax.plot([odom_poses[i][0] ,odom_poses[j][0]], [odom_poses[i][1], odom_poses[j][1]], color = c, alpha = 0.2)
                     continue
                 
-                # If no estimate for j, compute from the relative transform form i.
+                # If no estimate for j, compute from the relative transform from i.
                 else:
-                    R = np.array([[np.cos(poses[i][2]), -np.sin(poses[i][2])], 
-                                [np.sin(poses[i][2]), np.cos(poses[i][2])]])
+                    R = np.array([[np.cos(odom_poses[i][2]), -np.sin(odom_poses[i][2])], 
+                                [np.sin(odom_poses[i][2]), np.cos(odom_poses[i][2])]])
 
-                    trans_j = np.array([[poses[i][0]], [poses[i][1]]]) + R.dot(np.array([[dx], [dy]]))
-                    rot_j = poses[i][2] + dt
-                    poses[j] = (trans_j[0][0], trans_j[1][0], rot_j)
+                    trans_j = np.array([[odom_poses[i][0]], [odom_poses[i][1]]]) + R.dot(np.array([[dx], [dy]]))
+                    rot_j = odom_poses[i][2] + dt
+                    odom_poses[j] = (trans_j[0][0], trans_j[1][0], rot_j)
 
                     # Draw in red and if i is a landmark then draw blue line the landmark.
-                    ax.arrow(trans_j[0][0], trans_j[1][0], self.arrow_length * np.cos(rot_j), self.arrow_length * np.sin(rot_j), width=self.arrow_width, color = "r", alpha = 0.2)
+                    ax.arrow(trans_j[0][0], trans_j[1][0], self.arrow_length * np.cos(rot_j), self.arrow_length * np.sin(rot_j), width=self.arrow_width, color = "y", alpha = 0.2)
 
-                    ax.plot([poses[i][0] ,trans_j[0][0]], [poses[i][1], trans_j[1][0]], color = c, alpha = 0.2)
+                    ax.plot([odom_poses[i][0] ,trans_j[0][0]], [odom_poses[i][1], trans_j[1][0]], color = c, alpha = 0.2, label = "Odometry")
 
                     # Draw prior if it exists.
                     '''if j in self.prior_xyt:
                         x,y,t = self.prior_xyt[j]
                         ax.arrow(x, y, self.arrow_length * np.cos(t), self.arrow_length * np.sin(t), width=self.arrow_width, color = "g", alpha = 0.2)
                         ax.scatter([x],[y])'''
+
+                    # # Add measurement to maps figure.
+                    # if j in self.measurements:
+                    #     if self.measurements[j].right != 0:
+                    #         # Transformation for j in map frame.
+                    #         j_in_map = np.zeros((3,3))
+                    #         j_in_map[2,2] = 1
+                    #         j_in_map[:2, :2] = R
+                    #         j_in_map[:2,2:3] = trans_j    
+                    
+                    #         right_in_j_homo = np.array([[self.measurements[j].right, 0, 1]]).T
+                    #         left_in_j_homo  = np.array([[-self.measurements[j].left, 0, 1]]).T
+                    #         right_in_map = j_in_map.dot(right_in_j_homo)
+                    #         left_in_map = j_in_map.dot(left_in_j_homo)
+                    #         # Append (x,y) tuples to rights/lefts depending on the origin of the point.
+                    #         rights.append([right_in_map[0][0], right_in_map[1][0]])
+                    #         lefts.append([left_in_map[0][0], left_in_map[1][0]])
+                            # ax_maps.scatter([right_in_map[0][0]], [right_in_map[1][0]], color = c)
+                            # ax_maps.scatter([left_in_map[0][0]], [left_in_map[1][0]], color = c)
+        
+        odom_measurements_in_map = self.transform_measurements(odom_poses)
+        ax_maps.scatter([p[0] for p in odom_measurements_in_map], [p[1] for p in odom_measurements_in_map], color = c)
+        # ax_maps.scatter([p[0] for p in lefts], [p[1] for p in lefts], color = c)
+
+
         ax.set_aspect("equal")
+        ax_maps.set_aspect("equal")
     
 
 
@@ -200,7 +277,6 @@ class Graph():
                     c = "b"
                 else:
                     c = "r"
-                print(self.x_hat[2*i][0], self.x_hat[2*i+1][0], self.t_hat[i][0])
                 ax.arrow(self.x_hat[2*i][0], self.x_hat[2*i + 1][0], self.arrow_length * np.cos(self.t_hat[i][0]), self.arrow_length * np.sin(self.t_hat[i][0]), width=self.arrow_width, color = c)
                 
             # Go through all the edges and draw some lines (red between poses and blue for poses-to-landmarks.)
@@ -211,14 +287,67 @@ class Graph():
                 else:
                     c = 'r'
                     
-                ax.plot([self.x_hat[2*i][0] ,self.x_hat[2*j][0]], [self.x_hat[2*i + 1][0], self.x_hat[2*j + 1][0]], color = c)
-                ax.plot([self.x_hat[2*i][0] ,self.x_hat[2*j][0]], [self.x_hat[2*i + 1][0], self.x_hat[2*j + 1][0]], color = c)
+                ax.plot([self.x_hat[2*i][0] ,self.x_hat[2*j][0]], [self.x_hat[2*i + 1][0], self.x_hat[2*j + 1][0]], color = c, label="Smoothed")
+                gt_poses[i] = (self.x_hat[2*i][0], self.x_hat[2*i + 1][0], self.t_hat[i][0])
 
                 # Draw a line between the original and the optimized poses.
-                ax.plot( [self.x_hat[2*j][0] ,poses[j][0]], [self.x_hat[2*j + 1][0], poses[j][1]], color = 'k', linestyle = "dotted", alpha = 0.2)
+                # ax.plot( [self.x_hat[2*j][0] ,poses[j][0]], [self.x_hat[2*j + 1][0], poses[j][1]], color = 'k', linestyle = "dashdot", alpha = 0.2)
+        
+            # Draw the map from the optimized points.
+            gt_measurements_in_map = self.transform_measurements(gt_poses)
+            ax_maps.scatter([p[0] for p in gt_measurements_in_map], [p[1] for p in gt_measurements_in_map], color = c)
 
 
-        # ax.set_title("""self.ODOM_COST = %d
-        #                 self.LANDMARK_COST = %d
-        #                 self.ASSIGNMENT_COST = %d""" % (self.ODOM_NOISE, self.LANDMARK_NOISE, self.PRIOR_NOISE))
+        # Show the additional points, if those exist.
+        if additional_points:
+            ax.plot([x[0] for x in additional_points], [x[1] for x in additional_points], c = 'k')
+            ax.scatter([x[0] for x in additional_points], [x[1] for x in additional_points], c = 'k')
+
+        # ax.legend()
+        # ax.set_title("""self.ODOM_NOISE = %d
+        #                 self.LANDMARK_NOISE = %d
+        #                 self.PRIOR_NOISE = %d""" % (self.ODOM_NOISE, self.LANDMARK_NOISE, self.PRIOR_NOISE))
+
+        # Visualize the measurements on the map.
+        fig_rviz = plt.figure(2)
+        ax_rviz = fig_rviz.add_subplot(111)
+        self.ax_add_map(ax_rviz, self.map_path)
+        for pose in gt_measurements_in_map:
+            x_pixels, y_pixels = self.world_to_map(*pose[:2])
+            ax_rviz.scatter([x_pixels], [y_pixels], color = 'r')
+
         plt.show()
+
+
+   
+    def read_pgm(self, path):
+        raster = []
+        f = open(path, "rb")
+        f.readline()
+        f.readline()
+        (width, height) = [int(i) for i in f.readline().split()]
+        depth = int(f.readline())
+        assert depth <= 255
+        cellX = width
+        cellY = height
+        self.map_width_pixels = width
+        self.map_height_pixels = height
+
+        for y in range(height):
+            row = []
+            for x in range(width):
+                row.append(ord(f.read(1)))
+            raster.append(row)
+        f.close()
+
+        grey = np.array(raster)
+        grey[grey != 0] = 255
+        return grey
+
+    def world_to_map(self, x, y):
+        return (x / self.map_resolution + self.map_width_pixels/2, self.map_height_pixels/2 - y/self.map_resolution)
+
+    def ax_add_map(self, ax, map_path):
+        self.map_array_pixels = self.read_pgm(self.map_path)
+        ax.imshow(self.map_array_pixels, cmap="gray")
+        return ax
---
+++ src/slam/gtworld.py
@@ -0,0 +1,237 @@
+import numpy as np
+from gtgraph import Graph, RangeMeasurement
+import yaml
+import rosbag
+
+
+
+def deg_to_rad(deg):
+    rad = deg/360.0*2*np.pi
+    rad = ((rad + np.pi) % (2 * np.pi) ) - np.pi
+    return rad
+
+def quat_to_rad(x,y,z,w):
+    import math
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw = math.atan2(t3, t4)
+    return yaw
+
+
+def transform_matrix_from_tf_msg(msg):
+    """
+    Credit for base code: https://automaticaddison.com/how-to-convert-a-quaternion-to-a-rotation-matrix/ 
+    """
+    # Extract the values from Q
+    q0 = msg.transform.rotation.x
+    q1 = msg.transform.rotation.y
+    q2 = msg.transform.rotation.z
+    q3 = msg.transform.rotation.w
+     
+    # First row of the rotation matrix
+    r00 = 2 * (q0 * q0 + q1 * q1) - 1
+    r01 = 2 * (q1 * q2 - q0 * q3)
+    r02 = 2 * (q1 * q3 + q0 * q2)
+     
+    # Second row of the rotation matrix
+    r10 = 2 * (q1 * q2 + q0 * q3)
+    r11 = 2 * (q0 * q0 + q2 * q2) - 1
+    r12 = 2 * (q2 * q3 - q0 * q1)
+     
+    # Third row of the rotation matrix
+    r20 = 2 * (q1 * q3 - q0 * q2)
+    r21 = 2 * (q2 * q3 + q0 * q1)
+    r22 = 2 * (q0 * q0 + q3 * q3) - 1
+     
+    # 3x3 rotation matrix
+    matrix = np.array([[r00, r01, r02, msg.transform.translation.x],
+                       [r10, r11, r12, msg.transform.translation.y],
+                       [r20, r21, r22, msg.transform.translation.z],
+                       [  0,   0,   0, 1]])
+                            
+    return matrix
+
+# ==========================
+# RUNNING THE WORLD (girls).
+# ==========================
+if __name__ == "__main__":
+    bagfile_path = "/home/yoraish/Desktop/yorai/data/bagfiles/28-135-production_2022-02-14-05-00-06.bag"
+    beacons_yaml_path = '/indoorRobotics/indoor_maps/production/28/beacons.yaml'
+
+    # Keep the recent odom in map transform for computing initial estimates. map -> odom -> base_footprint.
+    recent_odom_in_map = None
+
+    # Keep points from the mission location (EKF) estimates.
+    ekf_basefootprint_in_map = []
+
+    g = Graph()
+    # Start with getting the beacons of the zone.
+    with open (beacons_yaml_path , "r") as stream:
+        try:
+            beacons_data = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    # Sort the beacons by name.
+    sorted_beacons = [beacon_key for beacon_key in beacons_data.keys()]
+    sorted_beacons.sort( key = lambda x: int(x.split("_")[-1]))
+    beacon_num = -1
+    for beacon_key in sorted_beacons:
+        new_beacon_num = int(beacon_key.split("_")[-1])
+
+        if new_beacon_num > beacon_num + 1:
+            while new_beacon_num > beacon_num + 1:
+                beacon_num += 1
+                g.add_edge(beacon_num, beacon_num, 0, 0, 0, [0,0,0])
+                print("Adding empty space for skipped beacon at %d." % beacon_num)
+            beacon_num += 1
+            print("Done adding empty. beacon_num at %d" % beacon_num)
+        else:
+            beacon_num = new_beacon_num
+
+        x, y, _, t, _ = beacons_data[beacon_key]
+        t = deg_to_rad(t)
+        print(beacon_num, x,y,t)
+
+        g.add_edge(beacon_num, beacon_num, x, y, t, [x,y,t])
+
+    
+
+
+    # Get poses. 
+    # Whenever we get a beacon sighting, if the most recent location pose was received less than 100ms ago, we connect an edge from the landmark to the pose.
+    recent_location_xyt = None
+    recent_location_time = None
+    recent_pose_ix = g.edges[-1].j
+    odom_is_rooted = False # We require one landmark observation to be added before adding odom.
+
+    # Landmark recently around.
+    ldx = None
+    ldy = None
+    ldt = None
+    recent_landmark_time = 0
+    lix = None
+    recent_sonar_right, recent_sonar_left = 0,0
+
+    recent_odom_time = 0
+    bag = rosbag.Bag(bagfile_path)
+    for topic, msg, t in bag.read_messages(topics=['/indoor/monitor/location', '/indoor/beacon/pose', '/indoor/mission/status', '/tf', '/sonar_right', '/sonar_left']):
+
+        if topic == "/indoor/monitor/location":
+            ekf_basefootprint_in_map.append((msg.location.x, msg.location.y))
+
+
+        if topic == "/tf":
+            if msg.transforms[0].child_frame_id == "odom" and msg.transforms[0].header.frame_id == "map":
+                recent_odom_in_map = transform_matrix_from_tf_msg(msg.transforms[0])
+
+            if msg.transforms[0].child_frame_id == "base_footprint" and msg.transforms[0].header.frame_id == "odom":
+                # if t.to_sec() - recent_odom_time < 2:
+                #     continue
+                recent_odom_time = t.to_sec()
+                x_msg = msg.transforms[0].transform.translation.x
+                y_msg = msg.transforms[0].transform.translation.y
+                t_msg = quat_to_rad(msg.transforms[0].transform.rotation.x, msg.transforms[0].transform.rotation.y, msg.transforms[0].transform.rotation.z, msg.transforms[0].transform.rotation.w)
+
+                # If no landmark seen yet, do not add an odom pose.
+                if odom_is_rooted == False:
+                    recent_location_xyt = [x_msg, y_msg, t_msg] # In odom frame.
+                    recent_location_time = t.to_sec()
+                    # Only root the odometry measurements when a link to a landmark is formed. This happens when a landmark measurement is recorded less than 100ms before an odom measurement.
+                    if t.to_sec() - recent_landmark_time < 0.01:
+                        # Add a new pose with an edge to this landmark. The new pose gets the ix recent_pose_ix + 1
+                        j = recent_pose_ix + 1
+                        recent_pose_ix = j
+                        recent_location_time = t.to_sec()
+                        odom_is_rooted = True
+
+                        # Transform the message pose (in odom frame) to be in map frame.
+                        basefootprint_in_odom = transform_matrix_from_tf_msg(msg.transforms[0])
+                        basefootprint_in_map = recent_odom_in_map.dot(basefootprint_in_odom)
+                        prior_xyt = [basefootprint_in_map[0][3], basefootprint_in_map[1][3], np.arctan2(basefootprint_in_map[2][1],basefootprint_in_map[1][1])]
+
+                        g.add_edge(lix, j, ldx, ldy, ldt, prior_xyt)
+                        print("Rooting odom.")
+
+                # If we have previous landmark positions and previous poses, then add an edge between the previous location and this new one.
+                else:
+                    i = recent_pose_ix
+                    j = recent_pose_ix + 1
+                    recent_pose_ix = j
+                    recent_location_time = t.to_sec()
+                    dx, dy, dt = g.relative_transform(*(recent_location_xyt + [x_msg, y_msg, t_msg]))
+
+                    # Transform the message pose (in odom frame) to be in map frame.
+                    basefootprint_in_odom = transform_matrix_from_tf_msg(msg.transforms[0])
+                    basefootprint_in_map = recent_odom_in_map.dot(basefootprint_in_odom)
+                    prior_xyt = [basefootprint_in_map[0][3], basefootprint_in_map[1][3], np.arctan2(basefootprint_in_map[2][1],basefootprint_in_map[1][1])]
+
+                    g.add_edge(i, j, dx, dy, dt, prior_xyt)
+                    recent_location_xyt = [x_msg, y_msg, t_msg]
+
+                    # Add a measurement for node j.
+                    m = RangeMeasurement(recent_sonar_right, recent_sonar_left)
+                    g.add_measurement(j, m)
+
+
+                    if t.to_sec() - recent_landmark_time < 0.01:
+                        # Add a new pose with an edge to this landmark. The new pose gets the ix recent_pose_ix + 1
+                        g.add_edge(lix, j, ldx, ldy, ldt, prior_xyt)
+
+                        # Remove this landmark from being considered again.
+                        recent_landmark_time = 0
+
+
+                        #######
+                        """""
+                        R = np.array([[np.cos(g.edges[lix].t), -np.sin(g.edges[lix].t)], 
+                                    [np.sin(g.edges[lix].t), np.cos(g.edges[lix].t)]])
+
+                        world_trans_j = np.array([[ g.edges[lix].x ], [ g.edges[lix].y]]) + R.dot(np.array([[ldx], [ldy]]))
+                        world_rot_j =  g.edges[lix].t + dt
+                        g.add""_edge(j, j, world_trans_j[0][0], world_trans_j[1][0], world_rot_j)""" # Assignment of odom node.
+                        ####
+
+
+                
+        if topic == "/indoor/beacon/pose":
+            # if msg.header.frame_id != "beacon0" and msg.header.frame_id != "beacon1":
+            # Transform from beacon to robot.
+            ldx = msg.pose.pose.position.x
+            ldy = msg.pose.pose.position.y
+            ldt = quat_to_rad(msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w)
+
+            recent_landmark_time = t.to_sec()
+            lix = int(msg.header.frame_id[len("beacon"):])
+
+        if topic == "/sonar_right":
+            if msg.range > 0:
+                recent_sonar_right = msg.range
+            else:
+                recent_sonar_right = 0
+
+        if topic == "/sonar_left":
+            if msg.range > 0:
+                recent_sonar_left = msg.range
+            else: 
+                recent_sonar_left = 0
+
+        ###################### STUPID TESTS ####################
+        # if len(g.edges) > 600:# 598:
+        #     break
+            # Add one artificial beacon measurement that is just fixing the last node in place.
+            # j = recent_pose_ix 
+            # ldx = 0
+            # ldy = 0
+            # ldt = 0
+            # lix = 6
+            # g.add_edge(lix, j, ldx, ldy, ldt)
+            # break
+        ###################### END STUPID TESTS ####################
+
+    bag.close()
+
+    print("Starting optimization on #edges=", len(g.edges))
+    g.optimize()
+    print("Starting visualization.")
+    g.visualize(ekf_basefootprint_in_map)
